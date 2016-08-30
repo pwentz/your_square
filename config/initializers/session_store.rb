9947feabbbed912bc07eb90c7eb7abfd08e2c9da
pwentz <patrick.w.wentz@gmail.com>
2016-08-30 14:54:27 -0600

    rename 127.0.0.1 to localhost

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_your_square_session', domain: 'http://127.0.0.1:3000'
+Rails.application.config.session_store :cookie_store,
+  key: '_your_square_session',
+  domain: 'http://localhost:3000'
